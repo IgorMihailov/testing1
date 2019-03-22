70f3d91688604fd1fa630b836f85f7039ea54289
IgorMihailov <45125141+IgorMihailov@users.noreply.github.com>
2019-03-22 10:29:10 +0300

    Update roots-tests.h

--- tests/roots-tests.h
+++ tests/roots-tests.h
@@ -24,6 +24,6 @@ TEST(rootsTest, less0)
 TEST(rootsTest, other)
 {
   ASSERT_EQ(equation(1,-6,5)->count, 2);
-  ASSERT_EQ(equation(1,-6,5)->x2, 5);
-  ASSERT_EQ(equation(1,-6,5)->x2, 1);    
+  ASSERT_EQ(equation(1,-6,5)->x2, 1);
+  ASSERT_EQ(equation(1,-6,5)->x2, 5);    
 }
